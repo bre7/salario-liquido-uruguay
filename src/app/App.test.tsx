f10fc7b481f6199950059b5371661135a8424990
bre7 <bre7@users.noreply.github.com>
2024-01-30 12:16:31 -0300

    setup tests with jest+nextjs

--- src/app/App.test.tsx
+++
@@ -1,9 +0,0 @@
-// import App from "./App"
-
-// test("renders learn react link", () => {
-//   render(<App />)
-//   const titleElement = screen.getByText(/demo123/)
-//   expect(titleElement).toBeInTheDocument()
-// })
-
-// TODO: add tests
