86712a2975139ed6a3a5cc0f9c22f583b2b9d063
bre7 <bre7@users.noreply.github.com>
2024-01-30 13:34:18 -0300

    enable vercel speed insights and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import generatedGitInfo from "#generated/git-info.json"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next"
 import Image from "next/image"
 import React from "react"
@@ -82,6 +84,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </a>
           </footer>
         </div>
+
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
